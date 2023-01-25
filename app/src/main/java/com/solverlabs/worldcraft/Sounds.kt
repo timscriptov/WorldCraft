5fe8b952df379aa68ff33ac8def3f45ff1728f99
Иван Тимашков <timscriptov@mail.ru>
2023-01-26 02:24:44 +0500

    ...

---
+++ app/src/main/java/com/solverlabs/worldcraft/Sounds.kt
@@ -0,0 +1,36 @@
+package com.solverlabs.worldcraft
+
+import com.solverlabs.worldcraft.R
+
+object Sounds {
+    const val DOOR_OPEN = R.raw.door_open
+    const val DOOR_CLOSE = R.raw.door_close
+    const val CHEST_OPEN = R.raw.chest_open
+    const val CHEST_CLOSE = R.raw.chest_close
+    const val COW = R.raw.cow
+    const val SHEEP = R.raw.sheep
+    const val PIG = R.raw.pig
+    @JvmField
+    val GRASS = intArrayOf(R.raw.grass1, R.raw.grass2, R.raw.grass3, R.raw.grass4)
+    @JvmField
+    val WOOD = intArrayOf(R.raw.wood1, R.raw.wood2, R.raw.wood3, R.raw.wood4)
+    @JvmField
+    val GRAVEL = intArrayOf(R.raw.gravel1, R.raw.gravel2, R.raw.gravel3, R.raw.gravel4)
+    @JvmField
+    val STONE = intArrayOf(R.raw.stone1, R.raw.stone2, R.raw.stone3, R.raw.stone4)
+    @JvmField
+    val ZOMBIE = intArrayOf(R.raw.zombie1, R.raw.zombie2, R.raw.zombie3)
+    @JvmField
+    val EXPLOSIVE = intArrayOf(R.raw.explode1, R.raw.explode2)
+    const val FUSE = R.raw.fuse
+    @JvmField
+    val FOOD = intArrayOf(R.raw.eat1, R.raw.eat2, R.raw.eat3)
+    const val BURP = R.raw.burp
+    @JvmField
+    val HUMAN_DAMAGE = intArrayOf(R.raw.hit1, R.raw.hit2, R.raw.hit3)
+    @JvmField
+    val ZOMBIE_HIT = intArrayOf(R.raw.zombie_hit1, R.raw.zombie_hit2)
+    const val ZOMBIE_DEATH = R.raw.zombie_death
+    const val GLASS = R.raw.glass
+    const val POP = R.raw.pop
+}
